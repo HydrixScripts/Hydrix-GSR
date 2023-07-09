b2337d07f4323e40fe281d53198d5b1df78dd5ae
/Hydrix_Scripts <84548316+imtorchedbud@users.noreply.github.com>
2023-07-09 12:34:01 -0400

    Update fxmanifest.lua

--- Hydrix-GSR-Main/fxmanifest.lua
+++ Hydrix-GSR-Main/fxmanifest.lua
@@ -1,8 +1,8 @@
 fx_version 'cerulean'
 game 'gta5'
 
-author 'Jordan.#2139'
-description 'GSR Test for CSRP'
+author 'Jordan.#2139 and </Hydrix_Scrpts>'
+description 'GSR Test'
 
 shared_script 'config.lua'
 client_script 'client.lua'
